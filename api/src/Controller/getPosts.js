c494cb57ca6764e7851f5711315cb1ef6776c00b
Luis Ballesteros <luisballesteros96@hotmail.com>
2022-09-17 15:07:57 -0500

    post join with users - correction-

--- api/src/Controller/getPosts.js
+++ api/src/Controller/getPosts.js
@@ -1,14 +1,16 @@
-const { Posts } = require("../db");
+const { Posts, Users } = require("../db");
 
 const getPosts = async (req, res) => {
-
   try {
-
-    const findPost = await Posts.findAll();
+    const findPost = await Posts.findAll({
+      include: {
+        //Si quiero incluir a otro modelo para que se vea, pongo  include
+        model: Users,
+        attributes: ["name", "email"],
+      },
+    });
     return res.json(findPost);
-
   } catch (error) {
-
     return res.status(500).send(error);
   }
 };
